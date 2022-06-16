e4c70fd427d2b3a5c9690fcecd0eb86ab9e92917
Abdul-Wahab-318 <wahabmaliq@gmail.com>
2022-06-16 17:04:39 +0500

    adds monthly expense per tag

--- src/components/expenseTags/ExpenseTags.js
+++ src/components/expenseTags/ExpenseTags.js
@@ -1,48 +1,116 @@
-import React from 'react'
+import React , { useEffect } from 'react'
 import './ExpenseTags.css'
 
 import { store } from '../../redux/store'
 
 export default function ExpenseTags() {
 
+  let currentMonth = new Date().getMonth()  
   let transactions = store.getState().transactions
-  let expenseTags = new Map()
+
+  //TOTAL EXPENSE 
+  let totalExpenseTags = new Map()
 
   transactions.map( transaction => {
 
     if ( transaction.type === "income" || transaction.type === "transfer" ) return 
 
-    if( expenseTags.has(transaction.to))
+    if( totalExpenseTags.has(transaction.to))
     {
-      expenseTags.set( transaction.to , expenseTags.get( transaction.to ) + transaction.amount )
+      totalExpenseTags.set( transaction.to , totalExpenseTags.get( transaction.to ) + transaction.amount )
     }
     else
     {
-      expenseTags.set(transaction.to , transaction.amount)
+      totalExpenseTags.set(transaction.to , transaction.amount)
     }
 
   })
+  //TOTAL EXPENSE END
+  
+  //MONTHLY EXPENSE
+  let monthlyTransactions = [] 
+  let monthlyExpenseMap = new Map()
+  let populateMonthlyTransactions = () => 
+  {
+    for ( let el of transactions )
+    {
+        if ( el.type != "expense" ) continue 
+
+        let transactionMonth = new Date(el.date).getMonth()
+
+        if ( transactionMonth == currentMonth )
+        monthlyTransactions.push( el )
+
+    }
+  }
+  populateMonthlyTransactions()
+ 
+
+  let populateMonthlyTransactionsMap = () => {
+
+    monthlyTransactions.map ( transaction => {
+
+      if ( monthlyExpenseMap.has( transaction.to ) )
+      {
+        monthlyExpenseMap.set( transaction.to , monthlyExpenseMap.get( transaction.to ) + transaction.amount )
+      }
+      else
+      {
+        monthlyExpenseMap.set( transaction.to , transaction.amount )
+      }
+    })
+
+  }
+
+  populateMonthlyTransactionsMap()
 
 
-  let expenseTagKeys  = [...expenseTags.keys()]
+  console.log( "Monthly expense are : " , [...monthlyExpenseMap.keys()])
+
+
+  let monthlyExpenseKeys = [ ...monthlyExpenseMap.keys() ]
+  let totalExpenseTagKeys  = [...totalExpenseTags.keys()]
+
   return (
     <section className="expense-tags">
         <div className="expense-tags-inner">
-          <div className="section-heading">
-            <h6 className='mb-0'>Total Expense Per Tag</h6>
-          </div>
-            <div className="expense-tags-cards">
-              {
-                expenseTagKeys.map( ( key , ind ) => {
-                  return (
-                  <div className='imp-info-card justify-content-between d-flex align-items-center' key = {ind} >
-                    <span className='percent-green fs-6 me-1'>{key} </span>
-                    <span> : </span>
-                    <span className='expense-amount fs-6 ms-1'>${expenseTags.get(key)}</span>
-                  </div>)
-                })
-              }
+
+        <section className="monthly-expenses">
+            <div className="section-heading">
+              <h6 className='mb-0'>Monthly Expense Per Tag</h6>
             </div>
+              <div className="expense-tags-cards">
+                {
+                  monthlyExpenseKeys.map( ( el , ind ) => {
+                    return (
+                    <div className='imp-info-card justify-content-between d-flex align-items-center' key={el}> 
+                      <span className='percent-green fs-6 me-1'>{el} </span>
+                      <span> : </span>
+                      <span className='expense-amount fs-6 ms-1'>${monthlyExpenseMap.get(el)}</span>
+                    </div>)
+                  })
+                }
+              </div>
+          </section>
+
+          <section className="total-expenses">
+            <div className="section-heading">
+              <h6 className='mb-0'>Total Expense Per Tag</h6>
+            </div>
+              <div className="expense-tags-cards">
+                {
+                  totalExpenseTagKeys.map( ( key , ind ) => {
+                    return (
+                    <div className='imp-info-card justify-content-between d-flex align-items-center'  key={ind} >
+                      <span className='percent-green fs-6 me-1'>{key} </span>
+                      <span> : </span>
+                      <span className='expense-amount fs-6 ms-1'>${totalExpenseTags.get(key)}</span>
+                    </div>)
+                  })
+                }
+              </div>
+          </section>
+
         </div>
     </section>
   )
